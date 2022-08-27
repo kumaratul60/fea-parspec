52d23c03579bf73293de582db3141bb9ac0c3992
Atul <atulreso1@gmail.com>
2022-08-27 13:17:39 +0530

    initial-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [mockData, setMockData] = useState([]);
+  const [input, setInput] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchMockData = async () => {
+      const fetchData = await fetch(
+        "http://www.mocky.io/v2/5ba8efb23100007200c2750c"
+      );
+      const response = await fetchData.json();
+      setMockData(response);
+      setIsLoading(false);
+      console.log("🚀 response", response);
+    };
+    fetchMockData().catch((err) => console.log("error", err));
+  }, [input]);
+
+  const onSubmitChange = (e) => {
+    e.preventDefault();
+    if (!input.trim()) {
+      alert("please type name/id");
+      return;
+    }
+
+    setInput("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <input
+        placeholder="Search users by ID, address, name..."
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
+      />
+      {isLoading ? (
+        <h3>Loading data...</h3>
+      ) : (
+        mockData
+          .filter((item) => {
+            if (input === "") {
+              return null;
+            } else if (item.name.toLowerCase().includes(input.toLowerCase())) {
+              return { item };
+            }
+          })
+          .map((data, index) => (
+            <div className="app__card" key={index}>
+              <p>{<b>{data.id}</b>}</p>
+              <span className="app__item">
+                <p>{`Name: ${data.name}`}</p>
+                <p>{`Address: ${data.address}`}</p>
+              </span>
+            </div>
+          ))
+      )}
     </div>
   );
 }
